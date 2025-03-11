e219c38340f8985898cbc760128d3a67cc362742
santyy <santiagovillanueva702@gmail.com>
2025-03-11 16:10:01 -0300

    Add tabla component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import { TablaComponent } from './tabla/tabla.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatInputModule
   ],
   providers: [
     provideClientHydration(withEventReplay())
@@ -18,3 +24,24 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
---
+++ src/app/tabla/tabla.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface Audiencia {
+  CUIJ: number;
+  caratula: string;
+  tipo: number;
+}
+
+const AUDIENCIA_DATA: Audiencia[] = [
+  {CUIJ: 1, caratula: 'Cara Tula', tipo: 1},
+  {CUIJ: 2, caratula: 'Cara Tula', tipo: 2},
+  {CUIJ: 3, caratula: 'Cara Tula', tipo: 1}
+];
+
+@Component({
+  selector: 'app-tabla',
+  standalone: false,
+  templateUrl: './tabla.component.html',
+  styleUrl: './tabla.component.css'
+})
+export class TablaComponent {
+  displayedColumns: string[] = ['CUIJ', 'caratula', 'tipo', 'accion'];
+  dataSource = new MatTableDataSource(AUDIENCIA_DATA);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+}
