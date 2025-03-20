1d6a8ef54ad67870727ac2826fcb3439c16a4358
santyy <santiagovillanueva702@gmail.com>
2025-03-19 23:34:26 -0300

    Add expand information

--- src/app/tabla/tabla.component.ts
+++ src/app/tabla/tabla.component.ts
@@ -14,6 +14,7 @@ import { AudienciasComponent } from '../audiencias/audiencias.component';
 export class TablaComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['CUIJ', 'caratula', 'tipo', 'acciones'];
   dataSource = new MatTableDataSource(AUDIENCIA_DATA);
+  expandedRow: Audiencias | null = null;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
@@ -74,7 +75,10 @@ export class TablaComponent implements OnInit, AfterViewInit {
       this.dataSource.data = this.dataSource.data.filter(a => a.CUIJ !== audiencia.CUIJ);
     }
   }
-  
+
+  toggleRow(row: any) {
+    this.expandedRow = this.expandedRow === row ? null : row;
+  }
 }
 
 
@@ -82,32 +86,35 @@ export interface Audiencias {
   CUIJ: number;
   caratula: string;
   tipo: number;
+  fecha: string;
+  horaInicio: string;
+  duracion: number;
 }
 
 const AUDIENCIA_DATA: Audiencias[] = [
-  { CUIJ: 556, caratula: 'Cara Tula 1', tipo: 3 },
-  { CUIJ: 209, caratula: 'Cara Tula 2', tipo: 4 },
-  { CUIJ: 284, caratula: 'Cara Tula 3', tipo: 3 },
-  { CUIJ: 881, caratula: 'Cara Tula 4', tipo: 2 },
-  { CUIJ: 740, caratula: 'Cara Tula 5', tipo: 4 },
-  { CUIJ: 251, caratula: 'Cara Tula 6', tipo: 2 },
-  { CUIJ: 843, caratula: 'Cara Tula 7', tipo: 3 },
-  { CUIJ: 588, caratula: 'Cara Tula 8', tipo: 3 },
-  { CUIJ: 252, caratula: 'Cara Tula 9', tipo: 2 },
-  { CUIJ: 169, caratula: 'Cara Tula 10', tipo: 1 },
-  { CUIJ: 930, caratula: 'Cara Tula 11', tipo: 3 },
-  { CUIJ: 272, caratula: 'Cara Tula 12', tipo: 1 },
-  { CUIJ: 299, caratula: 'Cara Tula 13', tipo: 1 },
-  { CUIJ: 467, caratula: 'Cara Tula 14', tipo: 2 },
-  { CUIJ: 717, caratula: 'Cara Tula 15', tipo: 2 },
-  { CUIJ: 191, caratula: 'Cara Tula 16', tipo: 2 },
-  { CUIJ: 470, caratula: 'Cara Tula 17', tipo: 1 },
-  { CUIJ: 283, caratula: 'Cara Tula 18', tipo: 3 },
-  { CUIJ: 483, caratula: 'Cara Tula 19', tipo: 4 },
-  { CUIJ: 584, caratula: 'Cara Tula 20', tipo: 3 },
-  { CUIJ: 379, caratula: 'Cara Tula 21', tipo: 3 },
-  { CUIJ: 874, caratula: 'Cara Tula 22', tipo: 2 },
-  { CUIJ: 887, caratula: 'Cara Tula 23', tipo: 4 },
-  { CUIJ: 885, caratula: 'Cara Tula 24', tipo: 1 },
-  { CUIJ: 548, caratula: 'Cara Tula 25', tipo: 2 }
+  { CUIJ: 556, caratula: 'Cara Tula 1', tipo: 3, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 209, caratula: 'Cara Tula 2', tipo: 4, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 284, caratula: 'Cara Tula 3', tipo: 3, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 881, caratula: 'Cara Tula 4', tipo: 2, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 740, caratula: 'Cara Tula 5', tipo: 4, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 251, caratula: 'Cara Tula 6', tipo: 2, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 843, caratula: 'Cara Tula 7', tipo: 3, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 588, caratula: 'Cara Tula 8', tipo: 3, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 252, caratula: 'Cara Tula 9', tipo: 2, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 169, caratula: 'Cara Tula 10', tipo: 1, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 930, caratula: 'Cara Tula 11', tipo: 3, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 272, caratula: 'Cara Tula 12', tipo: 1, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 299, caratula: 'Cara Tula 13', tipo: 1, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 467, caratula: 'Cara Tula 14', tipo: 2, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 717, caratula: 'Cara Tula 15', tipo: 2, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 191, caratula: 'Cara Tula 16', tipo: 2, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 470, caratula: 'Cara Tula 17', tipo: 1, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 283, caratula: 'Cara Tula 18', tipo: 3, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 483, caratula: 'Cara Tula 19', tipo: 4, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 584, caratula: 'Cara Tula 20', tipo: 3, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 379, caratula: 'Cara Tula 21', tipo: 3, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 874, caratula: 'Cara Tula 22', tipo: 2, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 887, caratula: 'Cara Tula 23', tipo: 4, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 885, caratula: 'Cara Tula 24', tipo: 1, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 },
+  { CUIJ: 548, caratula: 'Cara Tula 25', tipo: 2, fecha: '2024-03-19', horaInicio: '10:00', duracion: 60 }
 ];
